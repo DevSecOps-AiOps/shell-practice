2c66dda1dad4df2c1f420121db37594b4a91ba10
Chinnu891 <chinnusali891@gmail.com>
2026-06-07 13:25:20 +0530

    04-variables.sh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -9,14 +9,4 @@ echo "$PERSON1: Hey, $PERSON2 I am serious"
 echo "$PERSON2: Hey, $PERSON1, We are also serious"
 
 
-#To Run the script use below command
-# sh 02-conversation.sh Russia Ukraine 
-# Arguments passed are stored in special variables such as $1, $2, $3, ....., $9, ${10}
 
-PERSON1=$1
-PERSON2=$2
-
-echo "$PERSON1: Hey $PERSON2, we are going blast you!"
-echo "$PERSON2: Yes, Please you are welcome "
-echo "$PERSON1: Hey, $PERSON2 I am serious"
-echo "$PERSON2: Hey, $PERSON1, We are also serious"
---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/blash
+
+echo "Enter Your UserName:"
+read username               # Here username is a variable, whatever you enter in a variable will get stored in it. using $username we can print the value of a variable
+
+echo "My username is $username"
+
+echo "Please Enter Your Password:"
+read -s password           # Here -s is used to enter the password without displaying it on the screen
+echo " Password is : $password
